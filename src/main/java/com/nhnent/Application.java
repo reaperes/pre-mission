8f02b75b1ed60cb0cc3d0fa6827f4e971eb5ee9d
reaperes <emerald105@hanmail.net>
2014-12-21 21:22:00 +0900

    jsp hello world #1

--- src/main/java/com/nhnent/Application.java
+++ src/main/java/com/nhnent/Application.java
@@ -1,13 +1,18 @@
 package com.nhnent;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-@ComponentScan
-@EnableAutoConfiguration
-public class Application {
-    public static void main(String[] args) {
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/com/nhnent/controller/MainController.java
+++ src/main/java/com/nhnent/controller/MainController.java
@@ -1,13 +1,18 @@
 package com.nhnent.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
-@RequestMapping("/")
+import java.util.Date;
+import java.util.Map;
+
+@Controller
 public class MainController {
     @RequestMapping("/")
-    public String hello() {
-        return "hello world";
+    public String hello(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", "Hello, world");
+        return "welcome";
     }
 }
